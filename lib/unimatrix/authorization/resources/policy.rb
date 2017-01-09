39d90da75666dd9daf29a23f00039714bdacedb9
Asher Kory <asher.kory@gmail.com>
2017-01-09 12:16:01 -0500

    Remove unnecessary type_name from policy resource

--- lib/unimatrix/authorization/resources/policy.rb
+++ lib/unimatrix/authorization/resources/policy.rb
@@ -8,7 +8,6 @@ module Unimatrix::Authorization
     field     :resource
     field     :realm_uuid
     field     :actions
-    field     :type_name
 
     has_one   :resources
     has_one   :resource_servers
