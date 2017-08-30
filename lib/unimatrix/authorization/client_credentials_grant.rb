06dee231096f2dfdfe075ef7ecc52572e2224d52
zerogee00 <david@bedrocket.com>
2017-08-30 13:21:16 -0400

    change options check to symbol

--- lib/unimatrix/authorization/client_credentials_grant.rb
+++ lib/unimatrix/authorization/client_credentials_grant.rb
@@ -9,6 +9,7 @@ module Unimatrix
       end
 
       def request_token( options = {} )
+        binding.pry
         uri      = URI.parse( "#{ ENV[ 'KEYMAKER_URL' ] }/token" )
         params   = { "grant_type" => "client_credentials" }
         http     = Net::HTTP.new( uri.host, uri.port )
@@ -26,11 +27,12 @@ module Unimatrix
             body = JSON.parse( response.body )
             body = body[ 'token' ] if body[ 'token' ].present?
             result = body[ 'access_token' ] rescue nil
+            binding.pry
             if body.include?( 'error' )
                puts "ERROR: #{ body }"
                return body
             end
-            if options[ 'include_expiry' ] == true
+            if options[ :include_expiry ] == true
               body.delete( 'token_type' ) if body[ 'token_type' ].present?
               result = body
             end
